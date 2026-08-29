4bcc9e3432b111beecba45b0a2c035b9a323981d
manjunath395 <82149133+manjunath395@users.noreply.github.com>
2026-08-29 21:34:00 +0530

    Add variables for AWS region and S3 bucket

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -1,1 +1,20 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
 
+variable "bucket_name" {
+  description = "S3 bucket name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "versioning" {
+  description = "S3 bucket versioning status"
+  type        = string
+  default     = "Enabled"
+}
