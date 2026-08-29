2dd3615985f80ddfb5d4ee0095f90f9153089991
manjunath395 <82149133+manjunath395@users.noreply.github.com>
2026-08-29 21:29:41 +0530

    Add S3 module configuration to dev environment

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,1 +1,12 @@
+module "s3" {
+  source = "../../modules/s3"
 
+  bucket_name = var.bucket_name
+  environment = var.environment
+  versioning  = var.versioning
+
+  tags = {
+    Project = "ci-cd-demo-web-app"
+    Owner   = "DevOps"
+  }
+}
